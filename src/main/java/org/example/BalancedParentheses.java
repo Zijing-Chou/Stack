c7eb3fbd860d661963de00f2ad9e331b585e00f7
Chou Zijing <X00217230216@njit.edu.cn>
2024-03-15 15:52:45 +0800

    Initial commit

---
+++ src/main/java/org/example/BalancedParentheses.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Stack;
+
+class BalancedParentheses{
+
+ Stack<Character> stack = new Stack<>();
+
+ public void balanceParentheses(String expression){
+ for(char bracket: expression.toCharArray()){
+ if(bracket == '{' || bracket =='(' || bracket =='['){
+ stack.push(bracket);
+ }else if (bracket == '}' || bracket == ')' || bracket == '}'){
+ if(stack.isEmpty() || !isItBalanced(stack.pop(), bracket)){
+ System.out.println("The expression is not balanced");
+ return;
+ }
+ }
+}
+ System.out.println("The expression is balanced");
+ }
+
+ public boolean isItBalanced(char open, char close){
+ if(open == '{' && close == '}'){
+ return true;
+ }else if (open == '(' && close == ')'){
+ return true;
+ }else if (open == '[' && close == ']'){
+ return true;
+ }
+ return false;
+ }
+
+ public void balancedParentheses(String expression) {
+ }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Please enter an expression using only parentheses:");
+        Scanner reader = new Scanner(System.in);
+
+        String expression = reader.nextLine();
+        BalancedParentheses balance = new BalancedParentheses();
+        balance.balancedParentheses(expression);
+        reader.close();
+
+    }
+
+}
